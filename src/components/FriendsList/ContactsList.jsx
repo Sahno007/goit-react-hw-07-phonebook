cd350847506e293873038c65ac148c61aa022f8c
Sahno007 <turnskin90@gmail.com>
2023-11-17 17:54:13 +0200

    upg

--- src/components/FriendsList/ContactsList.jsx
+++ src/components/FriendsList/ContactsList.jsx
@@ -1,30 +1,20 @@
 import ContactItem from './ContactItem';
 import { useSelector, useDispatch } from 'react-redux';
 import ContactsListStyled from './ContactsListStyle.styled';
-import { getContactsList, getLoading, getError } from 'redux/contactsSlice';
-import { getFilter } from 'redux/filterSlice';
+import { selectFilteredContacts, selectFilter, selectLoading, selectError } from '../../redux/selectors';
 import { fetchContacts } from '../../redux/contactsOperations';
 import { useEffect } from 'react';
 
 const ContactList = () => {
-  const contacts = useSelector(getContactsList);
-  const filter = useSelector(getFilter);
+  const contacts = useSelector(selectFilteredContacts);
+  const filter = useSelector(selectFilter);
   const dispatch = useDispatch();
-  const loading = useSelector(getLoading);
-  const error = useSelector(getError);
+  const loading = useSelector(selectLoading);
+  const error = useSelector(selectError);
 
   useEffect(() => {
     dispatch(fetchContacts());
   }, [dispatch]);
-
-  const getFilterContacts = () => {
-    const normalizedFilter = filter.toLowerCase().trim();
-    return contacts.filter(
-      contact =>
-        contact.name.toLowerCase().includes(normalizedFilter) ||
-        contact.phone.toLowerCase().includes(normalizedFilter)
-    );
-  };
 
   if (loading) {
     return <h2>Loading...</h2>;
@@ -40,13 +30,13 @@ const ContactList = () => {
     );
   }
 
-  if (filter && getFilterContacts().length === 0) {
+  if (filter && contacts.length === 0) {
     return <h2>We didn't find any contacts according to your search</h2>;
   }
 
   return (
     <ContactsListStyled>
-      {getFilterContacts().map(contact => (
+      {contacts.map(contact => (
         <ContactItem
           userName={contact.name}
           userNumber={contact.phone}
@@ -59,6 +49,71 @@ const ContactList = () => {
 };
 
 export default ContactList;
+
+
+
+
+// import ContactItem from './ContactItem';
+// import { useSelector, useDispatch } from 'react-redux';
+// import ContactsListStyled from './ContactsListStyle.styled';
+// import { getContactsList, getLoading, getError } from 'redux/contactsSlice';
+// import { getFilter } from 'redux/filterSlice';
+// import { fetchContacts } from '../../redux/contactsOperations';
+// import { useEffect } from 'react';
+
+// const ContactList = () => {
+//   const contacts = useSelector(getContactsList);
+//   const filter = useSelector(getFilter);
+//   const dispatch = useDispatch();
+//   const loading = useSelector(getLoading);
+//   const error = useSelector(getError);
+
+//   useEffect(() => {
+//     dispatch(fetchContacts());
+//   }, [dispatch]);
+
+//   const getFilterContacts = () => {
+//     const normalizedFilter = filter.toLowerCase().trim();
+//     return contacts.filter(
+//       contact =>
+//         contact.name.toLowerCase().includes(normalizedFilter) ||
+//         contact.phone.toLowerCase().includes(normalizedFilter)
+//     );
+//   };
+
+//   if (loading) {
+//     return <h2>Loading...</h2>;
+//   }
+
+//   if (error) {
+//     return <h2>{error}</h2>;
+//   }
+
+//   if (contacts.length === 0) {
+//     return (
+//       <h2>You haven't any contacts. Please add contacts to your phonebook</h2>
+//     );
+//   }
+
+//   if (filter && getFilterContacts().length === 0) {
+//     return <h2>We didn't find any contacts according to your search</h2>;
+//   }
+
+//   return (
+//     <ContactsListStyled>
+//       {getFilterContacts().map(contact => (
+//         <ContactItem
+//           userName={contact.name}
+//           userNumber={contact.phone}
+//           id={contact.id}
+//           key={contact.id}
+//         />
+//       ))}
+//     </ContactsListStyled>
+//   );
+// };
+
+// export default ContactList;
 
 
 
---
+++ src/redux/selectors.js
@@ -0,0 +1,18 @@
+import { createSelector } from 'reselect';
+
+export const selectContactsList = state => state.contactsList.contacts.contacts;
+export const selectLoading = state => state.contactsList.contacts.isLoading;
+export const selectError = state => state.contactsList.contacts.error;
+export const selectFilter = state => state.filter;
+
+export const selectFilteredContacts = createSelector(
+  [selectContactsList, selectFilter],
+  (contacts, filter) => {
+    const normalizedFilter = filter && typeof filter === 'string' ? filter.toLowerCase().trim() : '';
+    return contacts.filter(
+      contact =>
+        contact.name.toLowerCase().includes(normalizedFilter) ||
+        contact.phone.toLowerCase().includes(normalizedFilter)
+    );
+  }
+);
