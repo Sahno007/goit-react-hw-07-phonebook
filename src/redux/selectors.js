9c7e78e508ac829df7afcb4e984e2000e2d99ceb
Sahno007 <turnskin90@gmail.com>
2023-11-18 19:03:37 +0200

    upg

--- src/redux/selectors.js
+++ src/redux/selectors.js
@@ -3,7 +3,7 @@ import { createSelector } from 'reselect';
 export const selectContactsList = state => state.contactsList.contacts.contacts;
 export const selectLoading = state => state.contactsList.contacts.isLoading;
 export const selectError = state => state.contactsList.contacts.error;
-export const selectFilter = state => state.filter;
+export const selectFilter = state => state.filter.filter;
 
 export const selectFilteredContacts = createSelector(
   [selectContactsList, selectFilter],
